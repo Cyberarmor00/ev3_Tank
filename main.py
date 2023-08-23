30b1e87c25c54e278f8b93e5d292fc1b06a12bee
Tobi <tobias.fierz@bluewin.ch>
2023-08-23 10:04:01 +0200

    start with functions

--- main.py
+++ main.py
@@ -7,9 +7,24 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
-
-
 ev3 = EV3Brick()
+ds=DistantSensor(Port.S1)
+cs=ColorSensor(Port.S2)
+ts=TouchSensor(Port.S3)
 
-ev3.speaker.beep()
+myRobot.DriveBase(left_motor(Port.A), right_motor(port.B), wheel_diameter, axle_track)
+
+def programm1():
+    distanz = ds.distance()
+    myrobot.drive(250)
+    if distanz =< 200:
+        myrobot.stop()
+        myrobot.turn(120)
+
+def programm2():
+    if button == False:
+        if ts.pressed=True:
+            button = True
+    if button == True:
+        if ts.pressed = True:
+            button = False
